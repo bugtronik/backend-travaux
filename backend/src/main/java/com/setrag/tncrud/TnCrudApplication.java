b49f28488a1f2576481f3f6c0983e493faac50f6
BugTekNik <bugtronik@gmail.com>
2023-11-07 11:13:29 +0100

    Install library and config properties

---
+++ backend/src/main/java/com/setrag/tncrud/TnCrudApplication.java
@@ -0,0 +1,13 @@
+package com.setrag.tncrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnCrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/setrag/tncrud/TnCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setrag.tncrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
